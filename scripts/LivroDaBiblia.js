22df354c64a10eb12c951b73bfe3112f37b6d4d8
Risi Ferraz <risiferraz@gmail.com>
2023-03-30 20:53:56 -0300

    Condicionamos a dica ao livro sorteado

--- scripts/LivroDaBiblia.js
+++ scripts/LivroDaBiblia.js
@@ -1,6 +1,13 @@
 class LivroDaBiblia {
-    constructor (id, src) {
+    constructor (id, src, dicaSRC) {
         this.id=id
         this.src=src
+        this.dicaSRC=dicaSRC
+    }
+    mostraLivro(){
+        const livroElementoHTML = document.getElementById("livro")
+        document.getElementById("nome-livro").textContent = this.id
+        livroElementoHTML.src = this.src
+        document.getElementById("dica").src = this.dicaSRC
     }
 }
