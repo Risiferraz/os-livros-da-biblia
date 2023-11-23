fc52fdfe3777bba899bc3ab80da37bcfa33f2033
Risi Ferraz <risiferraz@gmail.com>
2023-11-23 20:38:47 -0300

    Remover da lista de livros aqueles que já foram acertados - 1ª parte

--- scripts/index.js
+++ scripts/index.js
@@ -209,6 +209,11 @@ function autocomplete(inp, arr) {
     /*for each item in the array...*/
     for (i = 0; i < arr.length; i++) {
       /*check if the item starts with the same letters as the text field value:*/
+      let opacidade = "#000000"
+      if (listaDeLivrosJaAcertados.includes(arr[i])) {
+        opacidade = "#cccccc";
+
+      }
       if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
         /*create a DIV element for each matching element:*/
         b = document.createElement("DIV");
@@ -216,6 +221,7 @@ function autocomplete(inp, arr) {
         b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
         b.innerHTML += arr[i].substr(val.length);
         /*insert a input field that will hold the current array item's value:*/
+        b.style.color = opacidade
         b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
         /*execute a function when someone clicks on the item value (DIV element):*/
         b.addEventListener("click", function (e) {
